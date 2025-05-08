ca4ed757312681cc952cd00eca9beabf3a19e6c8
MaisaSh99 <maisa.shaqqur@gmail.com>
2025-05-09 01:45:01 +0300

    The Polybot Service - AWS Deployment solution

--- polybot/app.py
+++ polybot/app.py
@@ -7,7 +7,6 @@ app = flask.Flask(__name__)
 
 TELEGRAM_BOT_TOKEN = os.environ['TELEGRAM_BOT_TOKEN']
 BOT_APP_URL = os.environ['BOT_APP_URL']
-YOLO_SERVICE_URL = os.environ['YOLO_SERVICE_URL']  # ✅ Moved up here
 
 @app.route('/', methods=['GET'])
 def index():
@@ -20,5 +19,5 @@ def webhook():
     return 'Ok'
 
 if __name__ == "__main__":
-    bot = ImageProcessingBot(TELEGRAM_BOT_TOKEN, BOT_APP_URL, YOLO_SERVICE_URL)
+    bot = ImageProcessingBot(TELEGRAM_BOT_TOKEN, BOT_APP_URL)
     app.run(host='0.0.0.0', port=8443)
