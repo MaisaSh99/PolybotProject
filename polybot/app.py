76fe83f02c1f5430132767dcbab42740155c1878
MaisaSh99 <maisa.shaqqur@gmail.com>
2025-06-01 21:55:35 +0300

    updated bot and app and test files

--- polybot/app.py
+++ polybot/app.py
@@ -1,7 +1,7 @@
 import flask
 from flask import request
 import os
-from bot import ImageProcessingBot
+from polybot.bot import Bot, QuoteBot, ImageProcessingBot
 
 app = flask.Flask(__name__)
 
@@ -9,16 +9,9 @@ TELEGRAM_BOT_TOKEN = os.environ['TELEGRAM_BOT_TOKEN']
 BOT_APP_URL = os.environ['BOT_APP_URL']
 YOLO_SERVICE_URL = os.environ['YOLO_SERVICE_URL']
 
-# ✅ Move bot initialization to global scope
-bot = ImageProcessingBot(TELEGRAM_BOT_TOKEN, BOT_APP_URL, yolo_service_url=YOLO_SERVICE_URL)
-
 @app.route('/', methods=['GET'])
 def index():
     return 'Ok'
-
-@app.route('/health', methods=['GET'])
-def health():
-    return 'ok', 200
 
 @app.route(f'/{TELEGRAM_BOT_TOKEN}/', methods=['POST'])
 def webhook():
@@ -27,4 +20,5 @@ def webhook():
     return 'Ok'
 
 if __name__ == "__main__":
+    bot = ImageProcessingBot(TELEGRAM_BOT_TOKEN, BOT_APP_URL, YOLO_SERVICE_URL)
     app.run(host='0.0.0.0', port=8443)
